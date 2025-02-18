97513a0bbeb23251ffae798d19e518284f3f6caf
Piyush1532 <piyushadav503@gmail.com>
2025-02-18 15:14:52 +0530

    added routes

---
+++ Server/routes/authRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { loginUser, logoutUser, registerUser} from "../controller/authController.js"
+
+const authRouter=express.Router()
+
+
+
+authRouter.post("/register",registerUser)
+authRouter.post("/login",loginUser)
+authRouter.post("/logout",logoutUser)
+
+
+export default authRouter
