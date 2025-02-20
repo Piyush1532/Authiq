f4b82d1f39ad1e206e88364ad48c0c5d8884f912
Piyush1532 <piyushadav503@gmail.com>
2025-02-20 12:17:44 +0530

    added more routes

--- Server/routes/authRoute.js
+++ Server/routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { loginUser, logoutUser, registerUser} from "../controller/authController.js"
+import { isAuthenticated, loginUser, logoutUser, registerUser, sendVerifyOtp, vertifyEmail} from "../controller/authController.js"
+import userAuth from "../middleware/userAuth.js"
 
 const authRouter=express.Router()
 
@@ -8,6 +9,9 @@ const authRouter=express.Router()
 authRouter.post("/register",registerUser)
 authRouter.post("/login",loginUser)
 authRouter.post("/logout",logoutUser)
+authRouter.post("/send-verify-otp",userAuth,sendVerifyOtp)
+authRouter.post("/verify-account",userAuth,vertifyEmail)
+authRouter.post("/is-auth",userAuth,isAuthenticated)
 
 
 export default authRouter
