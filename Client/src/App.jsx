2392ea1351abf959113387104f1721b80d5f5c99
Piyush1532 <piyushadav503@gmail.com>
2025-02-26 13:58:25 +0530

    created routes and route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import EmailVerify from './Pages/EmailVerify'
+import ResetPassword from "./Pages/ResetPassword"
 const App = () => {
   return (
     <div>
-      hello
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/email-verify' element={<EmailVerify/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+      </Routes>
+
     </div>
   )
 }
