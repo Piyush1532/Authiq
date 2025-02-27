26bd577add675323c6e66c4c2605344aa8ee37f9
Piyush1532 <piyushadav503@gmail.com>
2025-02-27 14:39:38 +0530

    updated and completed

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
+const navigate=useNavigate()
+
   return (
     <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
       <img src={assets.header_img} alt=""  className='w-26 h-36 rounded-full mb-6'/>
@@ -11,7 +14,7 @@ const Header = () => {
 
       <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app</h2>
       <p className='mb-8 max-w-md'>Let's start with a quick product tour and we will have you up and running in no time!</p>
-      <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all'>Get Started</button>
+      <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all' onClick={()=>navigate("/login")} >Get Started</button>
     </div>
   )
 }
