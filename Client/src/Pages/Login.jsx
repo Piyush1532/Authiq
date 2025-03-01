4ae4fb4a451a1ec5831f780291d601813b8f07c6
Piyush1532 <piyushadav503@gmail.com>
2025-03-01 14:09:56 +0530

    working on api call

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContent } from '../Context/AppContext'
+import axios from "axios"
+import { toast } from 'react-toastify'
 
 const Login = () => {
  
 const navigate=useNavigate()
+
+const {backendUrl,setIsLoggedin}=useContext(AppContent)
+
 
   const [state,setState]=useState("Sign Up")
   const [name,setName]=useState("")
@@ -12,6 +18,37 @@ const navigate=useNavigate()
   const [password,setPassword]=useState("")
   
 
+const onSubmitHandler= async (e)=>{
+try {
+  e.preventDefault()
+
+  axios.defaults.withCredentials=true
+
+
+  if (state === "Sign Up") {
+    const {data}=await axios.post(backendUrl + "/api/auth/register",{name,email,password})
+    if(data.success){
+      setIsLoggedin(true)
+      navigate("/")
+
+    }else{
+      toast.error(data.message)
+    }
+  }else{
+    const {data}=await axios.post(backendUrl + "/api/auth/login",{email,password})
+    if(data.success){
+      setIsLoggedin(true)
+      navigate("/")
+
+    }else{
+      toast.error(data.message)
+    }
+  }
+
+} catch (error) {
+  toast.error(error.message)
+}
+}
 
 
   return (
@@ -25,7 +62,7 @@ const navigate=useNavigate()
 
 
 {/* Form Design */}
-<form>
+<form onSubmit={onSubmitHandler}>
 {state === "Sign Up"  && (
     <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
     <img src={assets.person_icon} alt="" />
