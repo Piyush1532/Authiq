ddb7adcee3de8f0acd797b333e34a20a67b5f3ad
Piyush1532 <piyushadav503@gmail.com>
2025-03-10 12:57:34 +0530

    added email html template file and used it

--- Server/controller/authController.js
+++ Server/controller/authController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodeMailer.js";
+import { EMAIL_VERIFY_TEMPLATE,PASSWORD_RESET_TEMPLATE } from "../config/emailTemplates.js";
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -123,7 +124,8 @@ export const sendVerifyOtp = async (req, res) => {
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Account Verification OTP",
-      text: `Your OTP is ${otp} . Verify your account using this OTP.`,
+      // text: `Your OTP is ${otp} . Verify your account using this OTP.`,
+      html:EMAIL_VERIFY_TEMPLATE.replace("{{otp}}",otp).replace("{{email}}",user.email)
     };
     await transporter.sendMail(mailOptions);
 
@@ -201,7 +203,8 @@ const otp = String(Math.floor(100000 + Math.random() * 900000));
       from: process.env.SENDER_EMAIL,
       to: user.email,
       subject: "Password Reset OTP",
-      text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password.`,
+      // text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password.`,
+      html:PASSWORD_RESET_TEMPLATE.replace("{{otp}}",otp).replace("{{email}}",user.email)
     };
 
     await transporter.sendMail(mailOptions);
